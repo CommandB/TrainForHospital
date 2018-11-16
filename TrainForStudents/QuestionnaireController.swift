070c8b2c9a10501acf2c78f8e0ca1066b03a15ad
陈海峰 <1397760231@qq.com>
2018-11-16 21:28:55 +0800

    bugFix:提交调查问卷后返回上一页

--- TrainForStudents/QuestionnaireController.swift
+++ TrainForStudents/QuestionnaireController.swift
@@ -106,7 +106,7 @@ class QuestionnaireController: MyBaseUIViewController{
                     case .success(let result):
                         let json = JSON(result)
                         if json["code"].intValue == 1 {
-                            myAlert(self, message: "提交成功!")
+                            self.dismiss(animated: true, completion: nil)
                         }else{
                             myAlert(self, title: "提交失败", message: json["msg"].stringValue)
                         }
