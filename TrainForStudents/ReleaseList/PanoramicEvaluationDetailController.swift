eb0f5534e7d80160275d294e234b3a330b309649
hhhhhhhhws <398970356@qq.com>
2018-11-28 12:35:50 +0800

    :wq

---
+++ TrainForStudents/ReleaseList/PanoramicEvaluationDetailController.swift
@@ -0,0 +1,9 @@
+//
+//  PanoramicEvaluationDetailController.swift
+//  TrainForStudents
+//
+//  Created by 黄玮晟 on 2018/11/27.
+//  Copyright © 2018 黄玮晟. All rights reserved.
+//
+
+import Foundation
